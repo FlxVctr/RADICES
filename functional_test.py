bce9fcd8b5647d28cdb001d0c9b20b8bdb8cfdb7
thiesben <b.thies@zeppelin-university.net>
2018-10-16 12:45:53 +0200

    start.py now handles missing, incomplete or bad keys.json files

--- functional_test.py
+++ functional_test.py
@@ -1,31 +1,32 @@
 # functional test for network collector
 import start
 import unittest
-import os
-from subprocess import check_output, STDOUT
+import json
+from json import JSONDecodeError
+
 
 class FirstUseTest(unittest.TestCase):
     """Functional test for first use of the program."""
 
     def test_starts_and_checks_for_necessary_input(self):
         # user starts program with `start.py`
-        #  response = str(check_output('python start.py', stderr=STDOUT, shell=True), encoding="ascii")
+        # response = str(check_output('python start.py', stderr=STDOUT,
+        # shell=True), encoding="ascii")
 
-        # There is a key file (json) or error
-        with self.assertRaises(FileNotFoundError):
-            start.function_for_testing()
-
-        # key file must contain they dictionary keys "consumer_key" and "consumer_secret"
-        #with self.assertRaises(KeyError):
-        #    start.function_for_testing()
+        # If there is no key file, a bad key file, or an incomplete key file,
+        # there will be an error.
+        with self.assertRaises(FileNotFoundError) and self.assertRaises(
+          JSONDecodeError) and self.assertRaises(KeyError):
+            start.read_key_file()
 
         # There is a seed file (csv with Twitter IDs) or error
-        self.assertIn('seed file not found', response)
+        # self.assertIn('seed file not found', response)
 
         # Seed file is not empty or error
 
         # There is a config with result-database details or error
-        # Program returns number of available keys and tests whether they can connect
+        # Program returns number of available keys and tests whether they can
+        # connect
         # Program returns number of seeds
         # Program tests connection to database (MySQL or BigQuery?)
 
--- start.py
+++ start.py
@@ -1,11 +1,15 @@
 import json
+from json import JSONDecodeError
 import os
 
-def function_for_testing():
+
+def read_key_file():
     try:
-        key_file = json.load(open(os.path.join("tests","keys.json"), "r"))
+        key_file = json.load(open(os.path.join("keys.json"), "r"))
     except FileNotFoundError:
         raise FileNotFoundError('"keys.json" could not be found')
-
+    except JSONDecodeError as e:
+        print('BAD JSON FILE. PLEASE CHECK IF "keys.json" IS FORMATTED CORRECTLY')
+        raise e
     if "consumer_key" not in key_file or "consumer_secret" not in key_file:
-        raise KeyError('"keys.json" does not contain the key "consumer_key" or "consumer_secret"')
+        raise KeyError('"keys.json" does not contain the dictionary keys "consumer_key" and/or "consumer_secret"')
