c77a7844081ac8f3dec19b7ee58898764da44a12
thiesben <b.thies@zeppelin-university.net>
2018-10-17 14:13:54 +0200

    add configreader and tests

--- configreader.py
+++ configreader.py
@@ -1,3 +1,15 @@
+import yaml
+
+
 class Config():
+
+    config_path = "config.yml"
+    config_template = "config_template.py"
+
     def __init__(self):
-        pass
+        try:
+            with open(self.config_path, 'r') as f:
+                config = yaml.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError('''Could not find "config.yml".\n
+            Please run "python3 make_config.py" or provide a config.yml''')
--- tests.py
+++ tests.py
@@ -1,26 +1,39 @@
 from make_db import DataBaseHandler
 import unittest
 import os
+from configreader import Config
 
 
 class DatabaseHandlerTest(unittest.TestCase):
 
     db_name = "NiceDB"
 
-    #def tearDown(self):
-    #    os.remove(self.db_name + ".db")
-
-    def test_no_database_(self):
-        DataBaseHandler()
+    def tearDown(self):
+        if os.path.isfile(self.db_name + ".db"):
+            os.remove(self.db_name + ".db")
 
     def test_database_handler_creates_database_from_given_name(self, db_name=db_name):
-
         dbh = DataBaseHandler()
         dbh.new_db(db_name=db_name)
         try:
             self.assertTrue(os.path.isfile(self.db_name + ".db"))
         except AssertionError:
             print("Database was not created!")
+
+
+class ConfigTest(unittest.TestCase):
+
+    def test_config_file_gets_read_incl_all_fields(self):
+        config_dict = {
+         'mysql': {
+            'dbtype': None,
+            'host': None,
+            'user': None,
+            'passwd': None,
+            'db_name': 'test_db'
+            }
+        }
+        self.assertEqual(Config().config, config_dict)
 
 
 if __name__ == "__main__":
