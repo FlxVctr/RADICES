66e1efe8f2e78b29d9247476db7bce236dfc1867
FlxVctr <fvmuench@gmail.com>
2018-10-17 16:59:11 +0200

    start collector

--- collector.py
+++ collector.py
@@ -1,4 +1,19 @@
+import pandas as pd
+import tweepy
+
+from setup import FileImport
+
+
 class Connection(object):
+    """Class that handles the connection to Twitter
+    """
+
+    def __init__(self):
+        self.credentials = FileImport.read_key_file()
+        if type(self.credentials[0]) != list:
+            self.auth = tweepy.OAuthHandler(self.credentials[0], self.credentials[1])
 
     def verify_credentials():
-        pass
+        """Connects to Twitter API to verify that credentials are working
+        """
+        raise tweepy.TweepError(reason="")
