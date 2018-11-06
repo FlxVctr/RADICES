b320ae550524057d95f08cc1d4c95a4a65a86c54
thiesben <b.thies@zeppelin-university.net>
2018-11-06 14:45:34 +0100

    add unittest.skip to one of the tests again

--- tests.py
+++ tests.py
@@ -177,7 +177,7 @@ class DataBaseHandlerTest(unittest.TestCase):
         self.assertIn("burned", sql_out)
         conn.close()
 
-    #@unittest.skip("This test drains API calls")
+    @unittest.skip("This test drains API calls")
     def test_dbh_write_friends_function_takes_input_and_writes_to_table(self):
         with open('config.yml', 'w') as f:
             yaml.dump(self.mock_sqlite_cfg, f, default_flow_style=False)
