cecce55fdc36bc60cd1abc9bf735a1da1c324bed
Felix Victor Münch <FlxVctr@users.noreply.github.com>
2018-11-21 16:03:06 +0100

    remove json dumps
    Co-Authored-By: thiesben <32708409+thiesben@users.noreply.github.com>

--- collector.py
+++ collector.py
@@ -406,7 +406,6 @@ class Collector(object):
             new = {k: v for k, v in flat.items() if k in select}
             json_list.append(new)
 
-        with open("Data/flat.json", "w") as f:
             json.dump(flat, f)
         with open("Data/new.json", "w") as f:
             json.dump(new, f)
