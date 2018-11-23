a83ad0e9852858cbb2eb8a815265a2dc8bc6a98f
FlxVctr <fvmuench@gmail.com>
2018-11-23 18:05:51 +0100

    refactor start for main loop to be wrapped in try except

--- start.py
+++ start.py
@@ -1,21 +1,20 @@
 import argparse
+import sys
+import time
+import traceback
+from exceptions import TestException
 from sys import stdout
+
+import pandas as pd
 
 from collector import Coordinator
 from setup import Config
 
 
-class TestException(Exception):
-    pass
-
-
-def main_loop(coordinator, test_fail=False, select=[], lang=None):
-
-    if test_fail is True:
-        raise TestException("Let's raise and shine!")
+def main_loop(coordinator, select=[], lang=None, test_fail=False):
 
     collectors = coordinator.start_collectors(select=select,
-                                              lang=lang)
+                                              lang=lang, fail=test_fail)
 
     stdout.write("\nstarting {} collectors\n".format(len(collectors)))
     stdout.flush()
@@ -36,7 +35,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--seeds', type=int, help="specify number of seeds", default=10)
     parser.add_argument('-l', '--language', help="specify language code of users to gather")
-    parser.add_argument('-t', '--test', help="test for 2 loops only", action="store_true")
+    parser.add_argument('-t', '--test', help="dev only: test for 2 loops only", action="store_true")
+    parser.add_argument(
+        '-f', '--fail', help="dev only: test unexpected exception", action="store_true")
 
     args = parser.parse_args()
 
@@ -55,9 +56,21 @@ if __name__ == "__main__":
 
         if args.test:
             k += 1
+            if k == 2:
+                args.fail = False
             if k == 3:
                 break
             stdout.write("\nTEST RUN {}\n".format(k))
             stdout.flush()
 
-            main_loop(coordinator, select=user_details_list, lang=args.language)
+        try:
+            main_loop(coordinator, select=user_details_list,
+                      lang=args.language, test_fail=args.fail)
+        except Exception:
+            stdout.write("Encountered unexpected exception:\n")
+            traceback.print_exc()
+            stdout.write("Retrying in 5 seconds.")
+            stdout.flush()
+            latest_seeds = list(pd.read_csv('latest_seeds.csv', header=None)[0].values)
+            coordinator = Coordinator(seed_list=latest_seeds)
+            time.sleep(5)
