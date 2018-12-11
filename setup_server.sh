ed2ba5de1618e9ed027a6cd16c16ef94726e7977
FlxVctr <fvmuench@gmail.com>
2018-12-11 16:23:44 +0100

    start mysql setup

---
+++ setup_server.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/chetankapoor/swap/master/swap.sh -O swap.sh
+sudo sh swap.sh 2G
+sudo apt-get update
+sudo apt-get install mysql-server
+mysql_secure_installation
