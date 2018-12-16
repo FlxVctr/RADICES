f8b12d38e3519d0408471863d46846341b843a09
FlxVctr <fvmuench@gmail.com>
2018-12-16 01:54:12 +0100

    don't return None if len == 0

--- collector.py
+++ collector.py
@@ -722,9 +722,6 @@ class Coordinator(object):
 
             query = f"SELECT {select} from user_details WHERE id IN {friends}"
             friend_detail = pd.read_sql(query, db_connection)
-            
-            if len(friend_detail) == 0:
-                return None
 
             return friend_detail
 
