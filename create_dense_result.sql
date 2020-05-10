37ace886e4743cc7b6a98b10759ccae047852984
FlxVctr <fvmuench@gmail.com>
2020-05-10 20:33:03 +0200

    add dense result query

---
+++ create_dense_result.sql
@@ -0,0 +1,21 @@
+SELECT DISTINCT source, target FROM friends WHERE source IN
+(SELECT DISTINCT T.id
+            FROM
+                (SELECT 
+                    result.source AS id
+                FROM
+                    result UNION SELECT 
+                    result.target AS id
+                FROM
+                    result) T)
+AND target IN
+(SELECT 
+                T.id
+            FROM
+                (SELECT 
+                    result.source AS id
+                FROM
+                    result UNION SELECT 
+                    result.target AS id
+                FROM
+                    result) T)
